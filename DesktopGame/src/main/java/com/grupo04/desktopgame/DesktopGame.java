977673f3e4f7570076f2fe8b6a793826eb17e000
Laura Wang Qiu <yiwang03@ucm.es>
2024-10-28 19:02:16 +0100

    Iconos en Android y Desktop

--- DesktopGame/src/main/java/com/grupo04/desktopgame/DesktopGame.java
+++ DesktopGame/src/main/java/com/grupo04/desktopgame/DesktopGame.java
@@ -3,12 +3,15 @@ package com.grupo04.desktopgame;
 import com.grupo04.desktopengine.DesktopEngine;
 import com.grupo04.gamelogic.scenes.TitleScene;
 
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 
 public class DesktopGame {
     public static void main(String[] args) {
         // Creacion de la ventana desktop
-        JFrame window = new JFrame("Puzzle Bobble");
+        JFrame window = new JFrame("Puzzle Bubble");
+        ImageIcon icon = new ImageIcon("./assets/PuzzleBubbleIcon.png");
+        window.setIconImage(icon.getImage());
         window.setSize(500, 900);
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         window.setIgnoreRepaint(true);
